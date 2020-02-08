3fb3c45b9099c9df923abb151deebb28ff852d7e
Colin-b <Colin-b@users.noreply.github.com>
2020-02-08 13:39:33 +0100

    Try to auto discover fixture

--- setup.py
+++ setup.py
@@ -30,9 +30,11 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Build Tools",
+        "Framework :: Pytest",
     ],
     keywords=["pytest", "testing", "mock", "httpx"],
     packages=find_packages(exclude=["tests*"]),
+    entry_points={"pytest11": ["pytest-httpx = pytest_httpx"]},
     install_requires=["httpx==0.11.*", "pytest==5.*"],
     extras_require={
         "testing": [
