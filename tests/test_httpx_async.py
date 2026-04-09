72712ce5a655f4d8b42e307fd73691ec050233c7
Colin-b <Colin-b@users.noreply.github.com>
2026-04-09 18:20:01 +0200

    Keep the number of test cases up to date

--- tests/test_httpx_async.py
+++ tests/test_httpx_async.py
@@ -77,16 +77,16 @@ async def test_url_query_string_matching(httpx_mock: HTTPXMock) -> None:
 async def test_url_query_params_partial_matching(httpx_mock: HTTPXMock) -> None:
     httpx_mock.add_response(
         url=httpx.URL("https://test_url"),
-        match_params={"a": ["1", "3"], "b": ANY, "c": "4", "d": ["5", ANY]},
+        match_params={"a": ["1", "3"], "b": ANY, "c": "4", "d": ["5", ANY], "e": [ANY]},
         is_reusable=True,
     )
 
     async with httpx.AsyncClient() as client:
-        response = await client.post("https://test_url?a=1&b=2&a=3&c=4&d=5&d=6")
+        response = await client.post("https://test_url?a=1&b=2&a=3&c=4&d=5&d=6&e=7")
         assert response.content == b""
 
         # Parameters order should not matter
-        response = await client.get("https://test_url?b=9&a=1&a=3&c=4&d=5&d=7")
+        response = await client.get("https://test_url?b=9&a=1&a=3&c=4&d=5&e=7&d=7")
         assert response.content == b""
 
 
--- tests/test_httpx_sync.py
+++ tests/test_httpx_sync.py
@@ -1,7 +1,7 @@
 import os
 import re
 from collections.abc import Iterable
-from typing import Union, Any
+from typing import Any
 from unittest.mock import ANY
 
 import httpx
@@ -68,16 +68,16 @@ def test_url_query_string_matching(httpx_mock: HTTPXMock) -> None:
 def test_url_query_params_partial_matching(httpx_mock: HTTPXMock) -> None:
     httpx_mock.add_response(
         url="https://test_url",
-        match_params={"a": ["1", "3"], "b": ANY, "c": "4", "d": ["5", ANY]},
+        match_params={"a": ["1", "3"], "b": ANY, "c": "4", "d": ["5", ANY], "e": [ANY]},
         is_reusable=True,
     )
 
     with httpx.Client() as client:
-        response = client.post("https://test_url?a=1&b=2&a=3&c=4&d=5&d=6")
+        response = client.post("https://test_url?a=1&b=2&a=3&c=4&d=5&d=6&e=7")
         assert response.content == b""
 
         # Parameters order should not matter
-        response = client.get("https://test_url?b=9&a=1&a=3&c=4&d=5&d=7")
+        response = client.get("https://test_url?b=9&a=1&a=3&c=4&d=5&e=7&d=7")
         assert response.content == b""
 
 
